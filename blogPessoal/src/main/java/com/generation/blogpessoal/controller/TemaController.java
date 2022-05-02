89dbdce2cf18be310d7a6e1dcebcf9d78de7c2f4
Fernando Brocco <fernandobroccox1@gmail.com>
2022-05-02 15:20:22 -0300

    Atualizacao Relacionamento entre as Classes

--- blogPessoal/src/main/java/com/generation/blogpessoal/controller/TemaController.java
+++ blogPessoal/src/main/java/com/generation/blogpessoal/controller/TemaController.java
@@ -2,6 +2,8 @@ package com.generation.blogpessoal.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -44,7 +46,7 @@ public class TemaController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<Tema> post(@RequestBody Tema tema) {
+	public ResponseEntity<Tema> post(@Valid @RequestBody Tema tema) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(tema));
 	}
 	
